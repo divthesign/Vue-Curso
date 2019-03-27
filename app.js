619fec7fb8706632c0683541a28a8d202a26da32
Daniel <verdudaniel7@gmail.com>
2019-03-27 15:48:27 +0100

    condicional 'if'

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = new  Vue({
             {nombre: 'Inicio', cantidad: 20},
             {nombre: 'Sobre nosotros', cantidad: 14},
             {nombre: 'Contacto', cantidad: 1}
+        ],
+        frutas: [
+            {nombre: 'Manzana', cantidad: 10},
+            {nombre: 'Platano', cantidad: 0},
+            {nombre: 'Pera', cantidad: 1},
         ]
     }
 })
